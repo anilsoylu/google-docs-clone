7f43e735becf37b49456262db9c1eed2d5fb4d3d
Anıl Soylu <56033298+anilsoylu@users.noreply.github.com>
2024-12-02 16:56:21 +0300

    updated: zustand editor store and tools

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -10,9 +10,38 @@ import Table from "@tiptap/extension-table"
 import TableCell from "@tiptap/extension-table-cell"
 import TableHeader from "@tiptap/extension-table-header"
 import TableRow from "@tiptap/extension-table-row"
+import Underline from "@tiptap/extension-underline"
+
+import { useEditoreStore } from "@/store/use-editor-store"
 
 export const Editor = () => {
+  const { setEditor } = useEditoreStore()
+
   const editor = useEditor({
+    onCreate({ editor }) {
+      setEditor(editor)
+    },
+    onDestroy() {
+      setEditor(null)
+    },
+    onUpdate({ editor }) {
+      setEditor(editor)
+    },
+    onSelectionUpdate({ editor }) {
+      setEditor(editor)
+    },
+    onTransaction({ editor }) {
+      setEditor(editor)
+    },
+    onFocus({ editor }) {
+      setEditor(editor)
+    },
+    onBlur({ editor }) {
+      setEditor(editor)
+    },
+    onContentError({ editor }) {
+      setEditor(editor)
+    },
     editorProps: {
       attributes: {
         style: "padding-left:56px; padding-right:56px;",
@@ -24,6 +53,7 @@ export const Editor = () => {
       StarterKit,
       Image,
       ImageResize,
+      Underline,
       TaskList,
       TaskItem.configure({
         nested: true,
